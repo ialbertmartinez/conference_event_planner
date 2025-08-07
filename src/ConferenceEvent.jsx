3b91414369c112da54a1cbffedecc220d6682385
Albert Martinez <ialbertmartinez@outlook.com>
2025-08-06 23:15:20 -0700

    fixed DOM order of 'add-on' elements

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -237,46 +237,44 @@ const ConferenceEvent = () => {
 							</div>
 
 							{/*Necessary Add-ons*/}
-							<div id="addons" className="venue_container container_main">
-								{avItems.map((item, index) => (
-									<div className="av_data venue_main" key={index}>
-										<div className="img">
-											<img src={item.img} alt={item.name} />
+							<div className="text">
+								<h1> Add-ons Selection</h1>
+							</div>
+							<div className="addons_selection">
+								<div 
+									id="addons" className="venue_container container_main">
+									{avItems.map((item, index) => (
+										<div 
+											className="av_data venue_main" key={index}>
+											<div className="img">
+												<img src={item.img} alt={item.name} />
+											</div>
+											<div className="text">
+												{item.name}
+											</div>
+											<div>
+												${item.cost}
+											</div>
+											<div className="addons_btn">
+												<button className="btn-warning" onClick={() => handleDecrementAvQuantity(index)}>
+													&ndash;
+												</button>
+												<span classNameName="quantity-value">
+													{item.quantity}
+												</span>
+												<button classNameName="btn-success" onClick={() => handleIncrementAvQuantity(index)}>
+													&#43;
+												</button>
+											</div>
 										</div>
-										<div className="text">
-											{item.name}
-										</div>
-										<div>
-											${item.cost}
-										</div>
-										<div className="addons_btn">
-											<button className="btn-warning" onClick={() => handleDecrementAvQuantity(index)}>
-												&ndash;
-											</button>
-											<span classNameName="quantity-value">
-												{item.quantity}
-											</span>
-											<button classNameName="btn-success" onClick={() => handleIncrementAvQuantity(index)}>
-												&#43;
-											</button>
-										</div>
-									</div>
-								))}
-
-								<div className="text">
-
-									<h1> Add-ons Selection</h1>
-
+									))}
 								</div>
-								<div className="addons_selection">
 
+								<div className="total_cost">Total Cost: ${avTotalCost}
 								</div>
-								<div className="total_cost">Total Cost: ${avTotalCost}</div>
-
 							</div>
 
 							{/* Meal Section */}
-
 							<div id="meals" className="venue_container container_main">
 
 								<div className="text">
